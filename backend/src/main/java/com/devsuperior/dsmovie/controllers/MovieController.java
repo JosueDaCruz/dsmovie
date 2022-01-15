55450663c1106226f5e6f5b7f29647502a025075
JosueDaCruz <nascido1980@gmail.com>
2022-01-15 14:01:39 -0300

    commit revisao

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -1,12 +1,11 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.services.MovieService;
@@ -19,14 +18,12 @@ public class MovieController {
 	private MovieService service;
 	
 	@GetMapping
-	public Page<MovieDTO> findAll(Pageable pageable){
-	return service.findAll(pageable);
-	
-}
+	public Page<MovieDTO> findAll(Pageable pageable) {
+		return service.findALL(pageable);
+	}
 	
 	@GetMapping(value = "/{id}")
-	public MovieDTO findById(@PathVariable Long id){
-	return service.findById(id);
-	
-}
+	public MovieDTO findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
 }
--- backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -2,10 +2,10 @@ package com.devsuperior.dsmovie.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
+import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.entities.Score;
 import com.devsuperior.dsmovie.entities.User;
